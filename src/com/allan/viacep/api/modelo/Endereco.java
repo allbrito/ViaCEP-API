e31520c992f98e0d81f67dbdc2f3763632d856ac
Allan <allbdesenvolvedor04@gmail.com>
2023-07-28 08:41:43 -0300

    Criação das classes de busca

---
+++ src/com/allan/viacep/api/modelo/Endereco.java
@@ -0,0 +1,4 @@
+package com.allan.viacep.api.modelo;
+
+public record Endereco(String cep, String logradouro, String complemento,
+                       String bairro, String localidade, String uf) {}
